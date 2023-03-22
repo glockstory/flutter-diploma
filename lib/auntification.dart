ecd1de0fceb3eddfca5c8be40d92f563d604b3b3
glockstory <danila1025@gmail.com>
2023-03-22 23:56:51 +0300

    initial project

---
+++ lib/auntification.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  _AuthPageState createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.teal],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_outline,
+              size: 80.0,
+              color: Colors.white,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.white),
+                icon: Icon(Icons.mail_outline, color: Colors.white),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.white),
+                icon: Icon(Icons.lock_outline, color: Colors.white),
+              ),
+              style: TextStyle(color: Colors.white),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,72 @@
+/*Для реализации навигации в Flutter можно использовать виджет Navigator. 
+Примерно так:
+
+1. Создать новую страницу (widget) в отдельном файле. 
+   Например, файл "SecondPage.dart":
+
+```
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Center(
+        child: Text("This is the Second Page"),
+      ),
+    );
+  }
+}
+```
+
+2. Создать главную страницу (widget) и добавить на неё кнопку для перехода на вторую страницу. 
+   Например, файл "MainPage.dart":
+
+```
+import 'package:flutter/material.dart';
+import 'SecondPage.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go to Second Page"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+```
+
+3. Добавить маршрут для главной страницы в функцию runApp(), создать экземпляр главного виджета и запустить приложение:
+
+```
+import 'package:flutter/material.dart';
+import 'MainPage.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Navigation Example',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainPage(),
+        '/second': (context) => SecondPage(),
+      },
+    ));
+```
+
+Это упрощенный пример навигации с двумя страницами, но вы можете добавить сколько угодно страниц и своих собственных маршрутов.
+*/
