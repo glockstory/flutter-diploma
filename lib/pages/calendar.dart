169c2b42c83ae0c878ed5ce1692b352567511735
glockstory <danila1025@gmail.com>
2023-03-27 21:26:20 +0300

    feat: add prototype calendar screen

---
+++ lib/pages/calendar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_final/pages/register.dart';
+import 'package:flutter_final/styles/textstyle.dart';
+import 'package:flutter_final/styles/buttonstyle.dart';
+import 'package:flutter/gestures.dart';
+
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(),
+    );
+  }
+}
