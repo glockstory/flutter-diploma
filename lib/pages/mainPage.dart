1512846b00155e7d3d6fccbb0c5a390a98871c55
glockstory <danila1025@gmail.com>
2023-03-31 18:58:24 +0300

    feat: add validator for email and password

--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_final/pages/register.dart';
 import 'package:flutter_final/styles/textstyle.dart';
 import 'package:flutter_final/styles/buttonstyle.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter_final/widgets/button.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -13,11 +14,21 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+  bool _validateEmail = false;
+  bool _validatePass = false;
 
+  final _formKey = GlobalKey<FormState>();
   final String logoImage = 'assets/logo.png';
 
   final ButtonStyle styleButton = ButtonStyle(
       minimumSize: MaterialStateProperty.all(const Size(double.infinity, 50)));
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,21 +43,58 @@ class _MainPageState extends State<MainPage> {
           ),
           Text('Войти в Routine Support', style: logoText),
           SizedBox(height: 15.0),
-          TextField(
-            controller: _emailController,
-            decoration:
-                InputDecoration(hintText: 'Email', border: borderTextField),
-          ),
-          SizedBox(height: 10.0),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            decoration: InputDecoration(
-              hintText: 'Password',
-              border: borderTextField,
-            ),
+          Form(
+            key: _formKey,
+            child: Column(children: [
+              TextFormField(
+                controller: _emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Поле должно быть заполнено';
+                  }
+                  if (!validateEmail(value)) {
+                    return 'Некорректный email';
+                  }
+                },
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  border: borderTextField,
+                  // errorText:
+                  //     _validateEmail ? null : 'Поле дожно быть заполнено',
+                  // errorBorder: _validateEmail
+                  //     ? OutlineInputBorder(
+                  //         borderSide: BorderSide(color: Colors.red),
+                  //         borderRadius: BorderRadius.circular(8.0),
+                  //       )
+                  //     : null,
+                  // focusedErrorBorder: _validateEmail
+                  //     ? OutlineInputBorder(
+                  //         borderSide: BorderSide(color: Colors.red),
+                  //         borderRadius: BorderRadius.circular(8.0),
+                  //       )
+                  //     : null,
+                ),
+              ),
+              SizedBox(height: 10.0),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Поле должно быть заполнено';
+                  }
+                  if (!validatePassword(value)) {
+                    return 'Пароль должен быть не меньше 3 символов';
+                  }
+                },
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  border: borderTextField,
+                ),
+              ),
+            ]),
           ),
           SizedBox(height: 10.0),
           Text.rich(TextSpan(children: [
@@ -63,15 +111,44 @@ class _MainPageState extends State<MainPage> {
                   })
           ])),
           SizedBox(height: 10.0),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => CalendarPage()));
-              },
-              child: Text('Войти', style: buttonText),
-              style: styleButton),
+          // ElevatedButton(
+          //     onPressed: () {
+          //       Navigator.push(context,
+          //           MaterialPageRoute(builder: (context) => CalendarPage()));
+          //     },
+          //     child: Text('Войти', style: buttonText),
+          //     style: styleButton),
+          MyButton(
+              label: 'Войти',
+              onTap: () {
+                if (_formKey.currentState!.validate()) {
+                  debugPrint('Success');
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CalendarPage()));
+                }
+                // setState(() {
+                //   _validateEmail = _emailController.text.isEmpty ? true : false;
+                //   _validatePass =
+                //       _passwordController.text.isEmpty ? true : false;
+                //   if (_validateEmail == false && _validatePass == false) {
+                //     Navigator.push(
+                //         context,
+                //         MaterialPageRoute(
+                //             builder: (context) => CalendarPage()));
+                //   }
+                // });
+              })
         ]),
       ),
     );
   }
+}
+
+bool validateEmail(String email) {
+  final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+  return emailRegex.hasMatch(email);
+}
+
+bool validatePassword(String password) {
+  return password.length >= 3;
 }
