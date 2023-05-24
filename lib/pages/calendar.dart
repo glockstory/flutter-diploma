cc4740dc08ccb65575f48f0dd35cba2247a90df7
glockstory <danila1025@gmail.com>
2023-05-24 20:22:29 +0300

    feat: add calendar page

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_final/pages/addActivity.dart';
+import 'package:flutter_final/pages/students.dart';
 import 'package:flutter_final/pages/mainPage.dart';
 import 'package:flutter_final/pages/register.dart';
 import 'package:flutter_final/styles/textstyle.dart';
 import 'package:flutter_final/styles/buttonstyle.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_final/widgets/button.dart';
+import 'package:flutter_final/widgets/gridViewWidget.dart';
 import 'package:flutter_final/widgets/inputField.dart';
-import 'package:table_calendar/table_calendar.dart';
+
 import '../utils.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   @override
@@ -16,179 +19,91 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  late final ValueNotifier<List<Event>> _selectedEvents;
-  CalendarFormat _calendarFormat = CalendarFormat.week;
-  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-  DateTime? _rangeStart;
-  DateTime? _rangeEnd;
-
-  @override
-  void initState() {
-    super.initState();
-    //_calendarController = CalendarController();
-    _selectedDay = _focusedDay;
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
-  }
-
-  @override
-  void dispose() {
-    _selectedEvents.dispose();
-    super.dispose();
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    // Implementation example
-    return kEvents[day] ?? [];
-  }
-
-  List<Event> _getEventsForRange(DateTime start, DateTime end) {
-    // Implementation example
-    final days = daysInRange(start, end);
-
-    return [
-      for (final d in days) ..._getEventsForDay(d),
-    ];
-  }
-
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    if (!isSameDay(_selectedDay, selectedDay)) {
-      setState(() {
-        _selectedDay = selectedDay;
-        _focusedDay = focusedDay;
-        _rangeStart = null; // Important to clean those
-        _rangeEnd = null;
-        _rangeSelectionMode = RangeSelectionMode.toggledOff;
-      });
-
-      _selectedEvents.value = _getEventsForDay(selectedDay);
-    }
-  }
-
-  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
-    setState(() {
-      _selectedDay = null;
-      _focusedDay = focusedDay;
-      _rangeStart = start;
-      _rangeEnd = end;
-      _rangeSelectionMode = RangeSelectionMode.toggledOn;
-    });
-
-    if (start != null && end != null) {
-      _selectedEvents.value = _getEventsForRange(start, end);
-    } else if (start != null) {
-      _selectedEvents.value = _getEventsForDay(start);
-    } else if (end != null) {
-      _selectedEvents.value = _getEventsForDay(end);
-    }
-  }
+  List<String> timeList = [
+    '6:00',
+    '7:00',
+    '8:00',
+    '9:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00',
+    '23:00'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Календарь'),
-        //leading: Icon(Icons.menu),
-      ),
-      drawer: Drawer(
-          child: SafeArea(
-              child: Column(
-        children: [
-          ListTile(
-            dense: true,
-            title: Text('Календарь'),
-            leading: Icon(Icons.calendar_month_rounded),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => CalendarPage()));
-            },
-          ),
-          ListTile(
-            dense: true,
-            title: Text('Дети'),
-            leading: Icon(Icons.people_rounded),
-            onTap: () {},
-          ),
-          ListTile(
-            dense: true,
-            title: Text('Выйти'),
-            leading: Icon(Icons.logout_rounded),
-            onTap: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => MainPage()));
-            },
-          )
-        ],
-      ))),
-      resizeToAvoidBottomInset: false,
-      body: Container(
-          child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-            TableCalendar<Event>(
-              firstDay: kFirstDay,
-              lastDay: kLastDay,
-              focusedDay: _focusedDay,
-              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-              rangeStartDay: _rangeStart,
-              rangeEndDay: _rangeEnd,
-              calendarFormat: _calendarFormat,
-              rangeSelectionMode: _rangeSelectionMode,
-              eventLoader: _getEventsForDay,
-              startingDayOfWeek: StartingDayOfWeek.monday,
-              calendarStyle: CalendarStyle(
-                // Use `CalendarStyle` to customize the UI
-                outsideDaysVisible: false,
-              ),
-              onDaySelected: _onDaySelected,
-              onRangeSelected: _onRangeSelected,
-              onFormatChanged: (format) {
-                if (_calendarFormat != format) {
-                  setState(() {
-                    _calendarFormat = format;
-                  });
-                }
-              },
-              onPageChanged: (focusedDay) {
-                _focusedDay = focusedDay;
+        appBar: AppBar(
+          title: Text('Календарь'),
+          //leading: Icon(Icons.menu),
+        ),
+        drawer: Drawer(
+            child: SafeArea(
+                child: Column(
+          children: [
+            ListTile(
+              dense: true,
+              title: Text('Календарь'),
+              leading: Icon(Icons.calendar_month_rounded),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CalendarPage()));
               },
             ),
-            const SizedBox(height: 8.0),
+            ListTile(
+              dense: true,
+              title: Text('Дети'),
+              leading: Icon(Icons.people_rounded),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => StudentsPage()));
+              },
+            ),
+            ListTile(
+              dense: true,
+              title: Text('Выйти'),
+              leading: Icon(Icons.logout_rounded),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MainPage()));
+              },
+            )
+          ],
+        ))),
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          children: [
+            Expanded(
+              child: SfCalendar(
+                view: CalendarView.day,
+              ),
+            ),
             MyButton(
                 label: '+ Создать',
                 onTap: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => AddActivity()));
                 }),
-            // Expanded(
-            //   child: ValueListenableBuilder<List<Event>>(
-            //     valueListenable: _selectedEvents,
-            //     builder: (context, value, _) {
-            //       return ListView.builder(
-            //         itemCount: value.length,
-            //         itemBuilder: (context, index) {
-            //           return Container(
-            //             margin: const EdgeInsets.symmetric(
-            //               horizontal: 12.0,
-            //               vertical: 4.0,
-            //             ),
-            //             decoration: BoxDecoration(
-            //               border: Border.all(),
-            //               borderRadius: BorderRadius.circular(12.0),
-            //             ),
-            //             child: ListTile(
-            //               onTap: () => print('${value[index]}'),
-            //               title: Text('${value[index]}'),
-            //             ),
-            //           );
-            //         },
-            //       );
-            //     },
-            //   ),
-            // ),
-          ]))),
-    );
+          ],
+        ));
   }
 }
+
+/*
+MyButton(
+                  label: '+ Создать',
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AddActivity()));
+                  }),
+*/
