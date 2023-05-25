bed20669057d9fa7d6cc311064550b9adb514f0d
glockstory <danila1025@gmail.com>
2023-05-25 23:27:37 +0300

    add: feat appointment in calendar

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -22,7 +22,36 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-  
+  CalendarDataSource _getCalendarDataSource() {
+    List<Appointment> appointments = <Appointment>[
+      Appointment(
+        startTime: DateTime.now(),
+        endTime: DateTime.now().add(Duration(hours: 2)),
+        subject: 'Полить цветы',
+        color: Colors.teal,
+      ),
+      Appointment(
+        startTime: DateTime.now(),
+        endTime: DateTime.now().add(Duration(hours: 2)),
+        subject: 'Помыть посуду',
+        color: Colors.teal,
+      ),
+      Appointment(
+        startTime: DateTime.now().add(Duration(hours: 4)),
+        endTime: DateTime.now().add(Duration(hours: 6)),
+        subject: 'Погулять',
+        color: Colors.teal,
+      ),
+      Appointment(
+        startTime: DateTime.now().add(Duration(days: 1)),
+        endTime: DateTime.now().add(Duration(days: 1, hours: 2)),
+        subject: 'Conference',
+        color: Colors.blue,
+      ),
+    ];
+    return _AppointmentDataSource(appointments);
+  }
+
   Future<List<dynamic>> getActivities() async {
     final prefs = await SharedPreferences.getInstance();
     final userId = prefs.getString('userId');
@@ -93,6 +122,84 @@ class _CalendarPageState extends State<CalendarPage> {
             Expanded(
               child: SfCalendar(
                 view: CalendarView.day,
+                dataSource: _getCalendarDataSource(),
+                showCurrentTimeIndicator: false,
+                onTap: (CalendarTapDetails details) {
+                  final appointment = details.appointments?.first;
+                  // final startTime = appointment.startTime;
+
+                  print(appointment);
+                  // print(startTime);
+                  //print('Appointments: $appointments');
+                },
+                appointmentBuilder: (context, calendarAppointmentDetails) {
+                  final appointment =
+                      calendarAppointmentDetails.appointments.first;
+                  final subject = appointment.subject;
+                  return Stack(children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(12),
+                          color: Color.fromARGB(255, 149, 237, 165)),
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '$subject',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            children: [
+                              //Text('1'),
+                              SizedBox(width: 4),
+                              //Icon(Icons.check_circle, color: Colors.green),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: InkWell(
+                        onTap: () {
+                          print('tapped');
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 118, 117, 117),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          padding: EdgeInsets.all(4),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text('0 / 3',
+                                  style: TextStyle(color: Colors.white)),
+                              SizedBox(width: 4),
+                              Icon(Icons.check, color: Colors.white),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]);
+                },
+                // appointmentTextStyle: TextStyle(
+                //     color: Colors.black,
+                //     fontSize: 35,
+                //     fontWeight: FontWeight.bold),
+                timeSlotViewSettings: TimeSlotViewSettings(
+                  timeIntervalHeight: 100,
+                  timeInterval: Duration(minutes: 60),
+                  //timeFormat: 'HH:mm'
+                ),
               ),
             ),
             MyButton(
@@ -114,3 +221,20 @@ MyButton(
                         MaterialPageRoute(builder: (context) => AddActivity()));
                   }),
 */
+// CalendarDataSource _getCalendarDataSource() {
+//   List<Appointment> appointments = <Appointment>[
+//     Appointment(
+//       startTime: DateTime.now(),
+//       endTime: DateTime.now().add(Duration(hours: 2)),
+//       subject: 'Meetings',
+//       color: Colors.teal,
+//     ),
+//   ];
+//   return _AppointmentDataSource(appointments);
+// }
+
+class _AppointmentDataSource extends CalendarDataSource {
+  _AppointmentDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
