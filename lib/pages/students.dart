91cd596a5655c7cf183f8fc5847a0a12019d610f
glockstory <danila1025@gmail.com>
2023-05-26 03:45:17 +0300

    feat: add page with a download students

--- lib/pages/students.dart
+++ lib/pages/students.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_final/models/student.dart';
 import 'package:flutter_final/pages/addActivity.dart';
+import 'package:flutter_final/pages/calendar.dart';
 import 'package:flutter_final/pages/mainPage.dart';
 import 'package:flutter_final/pages/register.dart';
 import 'package:flutter_final/styles/textstyle.dart';
@@ -8,7 +12,9 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter_final/widgets/button.dart';
 import 'package:flutter_final/widgets/gridViewWidget.dart';
 import 'package:flutter_final/widgets/inputField.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_final/widgets/studentCard.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 import '../utils.dart';
 
 class StudentsPage extends StatefulWidget {
@@ -16,7 +22,37 @@ class StudentsPage extends StatefulWidget {
   _StudentsPageState createState() => _StudentsPageState();
 }
 
+List<Key> studentKeys = List.generate(10, (_) => UniqueKey());
+
 class _StudentsPageState extends State<StudentsPage> {
+  List<Student> students = [];
+  int countStudents = 1;
+
+  Future<List<Student>> getStudents() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userId = prefs.getString('userId');
+    print(userId);
+    final Uri uri =
+        Uri.parse('http://10.0.2.2:3000/students/6230ceefdf1df472a675c92b');
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final List<dynamic> studentJson = json.decode(response.body);
+      students = studentJson.map((json) => Student.fromJson(json)).toList();
+      print(students);
+      print(students.length);
+      return students;
+    } else {
+      throw Exception('Failed to load records');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // вызов функции при запуске экрана
+    getStudents();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +69,8 @@ class _StudentsPageState extends State<StudentsPage> {
             title: Text('Календарь'),
             leading: Icon(Icons.calendar_month_rounded),
             onTap: () {
-              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CalendarPage()));
             },
           ),
           ListTile(
@@ -54,17 +91,35 @@ class _StudentsPageState extends State<StudentsPage> {
         ],
       ))),
       resizeToAvoidBottomInset: false,
-      body: Container(
-          padding: EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [],
-              )
-            ]),
-          )),
+      body: FutureBuilder<List<Student>>(
+        future: getStudents(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  child: StudentCardWidget(student: snapshot.data![index]),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return CircularProgressIndicator();
+        },
+        // itemCount: students.length,
+        // itemBuilder: (context, index) {
+        //   return Container(
+        //     child: StudentCardWidget(
+        //       student: students[index],
+        //     ),
+        //   );
+        // },
+      ),
+      floatingActionButton: MyButton(label: '+ Студент', onTap: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
     );
   }
 }
