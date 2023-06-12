932a3a7a51458374ead7deb25fd41b4b32d74d3d
glockstory <danila1025@gmail.com>
2023-06-13 02:48:53 +0300

    feat: delete debugLogo and start with first screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide DatePickerTheme;
+import 'package:flutter_final/pages/addActivity.dart';
 import 'package:flutter_final/pages/auntification.dart';
+import 'package:flutter_final/pages/calendar.dart';
 import 'package:flutter_final/pages/mainPage.dart';
 import 'package:flutter_final/pages/register.dart';
+import 'package:flutter_final/database/database.dart';
+import 'package:flutter_final/pages/testpage.dart';
 
-void main() {
-  runApp(MaterialApp(home: MainPage()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MainPage()));
 }
-
