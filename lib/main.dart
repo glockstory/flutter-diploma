6360f2080810918efa925337ad33f2145b0f0a25
glockstory <danila1025@gmail.com>
2023-06-13 05:12:22 +0300

    vendor: fix dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' hide DatePickerTheme;
-import 'package:flutter_final/pages/addActivity.dart';
-import 'package:flutter_final/pages/auntification.dart';
-import 'package:flutter_final/pages/calendar.dart';
 import 'package:flutter_final/pages/mainPage.dart';
-import 'package:flutter_final/pages/register.dart';
-import 'package:flutter_final/database/database.dart';
-import 'package:flutter_final/pages/testpage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
